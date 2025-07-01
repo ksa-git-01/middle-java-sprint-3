2f488196514781cad7f80d95bfbfa2cbe91f79e1
ksa <krutikov.s@gmail.com>
2025-07-01 21:02:19 +0300

    add dao postgre impl

---
+++ src/main/java/ru/yandex/practicum/dao/PostDaoPostgreRepository.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class PostDaoPostgreRepository implements PostDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public PostDaoPostgreRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Post> findAllWithPagination(Integer page, Integer size) {
+        Integer offset = size * page;
+        return jdbcTemplate.query("""
+                        SELECT id, title, content, likes, filename, created_at, updated_at 
+                        FROM post 
+                        ORDER BY id 
+                        LIMIT ? OFFSET ?
+                        """,
+                (rs, rowNum) -> new Post(
+                        rs.getLong("id"),
+                        rs.getString("title"),
+                        rs.getString("content"),
+                        rs.getInt("likes"),
+                        rs.getString("filename"),
+                        rs.getTimestamp("created_at").toLocalDateTime(),
+                        rs.getObject("updated_at", LocalDateTime.class)),
+                size,
+                offset
+        );
+    }
+}
