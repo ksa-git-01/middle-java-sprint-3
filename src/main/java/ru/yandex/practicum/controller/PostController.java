f70b307149661864f013d869ab0b0d924cf5069b
ksa <krutikov.s@gmail.com>
2025-07-02 16:38:38 +0300

    add pagination and page size parameters

--- src/main/java/ru/yandex/practicum/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/controller/PostController.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.yandex.practicum.dto.PostListItemDto;
 import ru.yandex.practicum.service.PostService;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class PostController {
@@ -17,10 +19,20 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public String getPosts(Model model) {
-        List<PostListItemDto> postListItems = service.getPostListItems(0, 25);
+    public String getPosts(@RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "10") int size,
+                           Model model) {
+        if (page < 0) {
+            page = 0;
+        }
+        if (!Set.of(10, 20, 50).contains(size)) {
+            size = 10;
+        }
+        List<PostListItemDto> postListItems = service.getPostListItems(page, size);
 
         model.addAttribute("posts", postListItems);
+        model.addAttribute("page", page);
+        model.addAttribute("size", size);
         return "posts";
     }
 }
